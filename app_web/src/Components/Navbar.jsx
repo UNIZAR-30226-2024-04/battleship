98bc151827013a2b95a98ee6506b54966dee7158
luis7ps <74595457+luis7ps@users.noreply.github.com>
2024-03-11 21:49:52 +0100

    NavBar 100% altura + color de fondo

--- app_web/src/Components/Navbar.jsx
+++ app_web/src/Components/Navbar.jsx
@@ -1,44 +1,42 @@
-import "./Navbar.css";
+
 import React from 'react';
 import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar'
 import { useNavigate } from 'react-router-dom';
+import battleshipImage from '../Images/bs_Logo.png';
 
 
 export function Navbar() {
     const navigate = useNavigate();
     return (
-        <Sidebar>
-            <Menu>
-                <MenuItem onClick={() => navigate('/')}>Home</MenuItem>
-                <MenuItem onClick={() => navigate('/fleet')}>Flota</MenuItem>
-                <MenuItem onClick={() => navigate('/settings')}>Ajustes</MenuItem>
-                <MenuItem onClick={() => navigate('/profile')}>Perfil</MenuItem>
-                <MenuItem onClick={() => navigate('/social')}>Social</MenuItem>
-            </Menu>
-        </Sidebar>
-    );
-}
-
-/*
-    const navigate = useNavigate();
-    return (
-        <div className="navBar">
-            <button onClick={() => navigate('/')}>Jugar</button>
-            <button onClick={() => navigate('/fleet')}> Flota</button>
-            <button onClick={() => navigate('/settings')}>Ajustes</button>
-            <button onClick={() => navigate('/profile')}>Perfil</button>
-            <button onClick={() => navigate('/social')}>Social</button>
+        <div style={{ display: "flex", height: "100vh" }}>
+            <Sidebar 
+            // Cambiar el color de fondo
+            style={{backgroundColor: '#1D3461'}}
+            // Modificar el ancho del sidebar
+            width="100px"
+            // Modificar el alto del sidebar
+            
+            >
+                <img src={battleshipImage} alt="logo" width="100px" height="100px" onClick={() => navigate('/')}></img>
+                <center onClick={() => navigate('/')}>Battleship</center>
+                <br></br>
+                <Menu
+                menuItemStyles={{
+                    button: ({ level, active, disabled }) => {
+                    // only apply styles on first level elements of the tree
+                    if (level === 0)
+                        return {
+                            color: disabled ? undefined : '#ffffff',
+                        };
+                    },
+                }}>
+                    <MenuItem onClick={() => navigate('/')}>Home</MenuItem>
+                    <MenuItem onClick={() => navigate('/fleet')}>Flota</MenuItem>
+                    <MenuItem onClick={() => navigate('/settings')}>Ajustes</MenuItem>
+                    <MenuItem onClick={() => navigate('/profile')}>Perfil</MenuItem>
+                    <MenuItem onClick={() => navigate('/social')}>Social</MenuItem>
+                </Menu>
+            </Sidebar>
         </div>
     );
-*/
-
-
-/*
-<div className="navBar">
-    <NavLink to="/">Jugar</NavLink>
-    <NavLink to="/fleet">Flota</NavLink>
-    <NavLink to="/settings">Ajustes</NavLink>
-    <NavLink to="/profile">Perfil</NavLink>
-    <NavLink to="/social">Social</NavLink>
-</div>
-*/
+}
