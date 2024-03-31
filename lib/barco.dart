0a88e0c4fb3392ae8c6289fac6b93778e3a270bd
RollerCoaster22 <806895@unizar.es>
2024-03-31 20:55:01 +0200

    Multiples barcos

--- lib/barco.dart
+++ lib/barco.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 class Barco {
-  static String? _nombre = "";
-  static Offset _barcoPosition = Offset(0.0, 0.0);
-  static double _barcoSize = 0.0;
-  static bool _esRotado = false;
-  static int _longitud = 0;
+  String _nombre = "barco";
+  Offset _barcoPosition = Offset(0.0, 0.0);
+  double _barcoSize = 0.0;
+  bool _esRotado = false;
+  int _longitud = 0;
 
   Barco(String nombre, Offset barcoPosition, int longitud, double barcoSize, bool esRotado) {
     _nombre = nombre;
@@ -13,14 +13,12 @@ class Barco {
     _longitud = longitud;
     _barcoSize = barcoSize;
     _esRotado = esRotado;
-    // Mostrar por consola
-    print("Barco: " + _nombre! + " en posición: " + _barcoPosition.toString() + " con " + _longitud.toString() + " partes y tamaño: " + _barcoSize.toString());
   }
 
   // Getters
-  String get nombre => _nombre!;
+  String get nombre => _nombre;
   Offset get barcoPosition => _barcoPosition;
-  int get numPartesBarco => _longitud;
+  int get longitud => _longitud;
   double get barcoSize => _barcoSize;
   bool get esRotado => _esRotado;
   double getWidth(double _casillaSize) => esRotado ? _casillaSize : _longitud * _casillaSize;
@@ -31,7 +29,7 @@ class Barco {
     _barcoPosition = barcoPosition;
   }
 
-  set numPartesBarco(int longitud) {
+  set longitud(int longitud) {
     _longitud = longitud;
   }
 
--- lib/jugar.dart
+++ lib/jugar.dart
@@ -1,4 +1,3 @@
-import 'dart:html';
 import 'package:flutter/material.dart';
 import 'comun.dart';
 import 'barco.dart';
@@ -11,31 +10,28 @@ class Jugar extends StatefulWidget {
 }
 
 class _JugarState extends State<Jugar> {
-  static int _numFilas = 0;
-  static int _numColumnas = 0;
+  static int _numFilas = 11;
+  static int _numColumnas = 11;
   Map<Barco, bool> _draggingStates = {};
   static double _casillaSize = 0.0;
   static double _boardSize = 0.0;
-  List<Barco> barcos = [];
+  static List<Barco> barcos = [];
 
   @override
   void initState() {
     super.initState();
-    _numFilas = 10;
-    _numColumnas = 10;
-    _boardSize = 360.0;
+    _boardSize = 341.0;
     _casillaSize = _boardSize / _numFilas;
-    //Barco barco1 = Barco('acorazado', Offset(0, 0), 3, 3*_casillaSize, false);
-    Barco barco2 = Barco('destructor_rotado', Offset(2.0, 2.0), 3, 3*_casillaSize, true);
-    //Barco barco3 = Barco('patrullero', Offset(3, 2), 2, 2*_casillaSize, false);
-    //Barco barco4 = Barco('portaaviones', Offset(5, 5), 2, 2*_casillaSize, false);
-    //Barco barco5 = Barco('submarino_rotado', Offset(6, 6), 2, 2*_casillaSize, true);
-    barcos = [barco2];
+    Barco barco1 = Barco('acorazado', Offset(1, 1), 3, 3*_casillaSize, false);
+    Barco barco2 = Barco('destructor_rotado', Offset(3, 3), 3, 3*_casillaSize, true);
+    Barco barco3 = Barco('patrullero', Offset(3, 2), 2, 2*_casillaSize, false);
+    Barco barco4 = Barco('portaaviones', Offset(5, 5), 2, 2*_casillaSize, false);
+    Barco barco5 = Barco('submarino_rotado', Offset(6, 6), 2, 2*_casillaSize, true);
+    barcos = [barco1, barco2, barco3, barco4, barco5];
     _draggingStates = {
       for (var barco in barcos) barco: false,
     };
   }
-
 
   Widget build(BuildContext context) {
     return Container(
@@ -51,7 +47,6 @@ class _JugarState extends State<Jugar> {
           children: [
             buildHeader(context),
             buildTitle('¡Coloca tu flota!', 28),
-            const Spacer(),
             _construirTableroConBarcos(),
             const Spacer(),
             buildActions(context),
@@ -61,46 +56,50 @@ class _JugarState extends State<Jugar> {
     );
   }
 
-
   Widget _construirTableroConBarcos() {
     return Stack(
       children: [
         SizedBox(
-          width: _boardSize, // Ancho del tablero
-          height: _boardSize, // Altura del tablero
+          width: _boardSize + _casillaSize, // Ancho del tablero con espacio adicional para la columna de coordenadas
+          height: _boardSize + _casillaSize, // Altura del tablero con espacio adicional para la fila de coordenadas
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: _buildTablero(),
           ),
         ),
-        for (Barco barco in barcos)
+        for (var barco in barcos)
           Positioned(
-            top: barco.barcoPosition.dy*_casillaSize,
-            left: barco.barcoPosition.dx*_casillaSize,
-            child: GestureDetector(
-              onPanStart: (details) {
-                setState(() {
-                  _draggingStates[barco] = true;
-                });
-              },
-              onPanUpdate: (details) {
-                setState(() {
-                  barco.barcoPosition += details.delta;
-                  barco.barcoPosition = _boundPosition(barco.barcoPosition, barco.barcoSize, barco.esRotado);
-                });
-              },
-              onPanEnd: (details) {
-                setState(() {
-                  _draggingStates[barco] = false;
-                });
-              },
-              child: Opacity(
-                opacity: _draggingStates[barco] ?? false ? 0.5 : 1.0,
-                child: Image.asset(
-                  'images/'+barco.nombre+'.png',
-                  width: barco.getWidth(_casillaSize),
-                  height: barco.getHeight(_casillaSize),
+            top: barco.barcoPosition.dy * _casillaSize,
+            left: barco.barcoPosition.dx * _casillaSize,
+            child: Column(
+              children: [
+                GestureDetector(
+                  onPanStart: (details) {
+                    setState(() {
+                      _draggingStates[barco] = true;
+                    });
+                  },
+                  onPanUpdate: (details) {
+                    setState(() {
+                      barco.barcoPosition += details.delta;
+                      barco.barcoPosition = _boundPosition(barco.barcoPosition, barco.getHeight(_casillaSize), barco.getWidth(_casillaSize));
+                    });
+                  },
+                  onPanEnd: (details) {
+                    setState(() {
+                      _draggingStates[barco] = false;
+                    });
+                  },
+                  child: Opacity(
+                    opacity: _draggingStates[barco] ?? false ? 0.5 : 1.0,
+                    child: Image.asset(
+                      'images/' + barco.nombre + '.png',
+                      width: barco.getWidth(_casillaSize),
+                      height: barco.getHeight(_casillaSize),
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
           ),
       ],
@@ -109,40 +108,73 @@ class _JugarState extends State<Jugar> {
 
   List<Widget> _buildTablero() {
     List<Widget> filas = [];
-    for (int i = 0; i < _numFilas; i++) {
-      filas.add(_buildFilaCasillas());
+    // Añade una fila adicional para las etiquetas de las coordenadas
+    filas.add(_buildFilaCoordenadas());
+    for (int i = 0; i < _numFilas - 1; i++) {
+      filas.add(_buildFilaCasillas(i));
     }
     return filas;
   }
 
-  Widget _buildFilaCasillas() {
-    List<Widget> casillas = [];
+  Widget _buildFilaCoordenadas() {
+    List<Widget> coordenadas = [];
+    // Etiqueta de columna vacía para compensar la columna de coordenadas
+    coordenadas.add(Container(
+      width: _casillaSize,
+      height: _casillaSize,
+    ));
+    // Etiquetas de columna
+    for (int j = 1; j <= _numColumnas; j++) {
+      coordenadas.add(
+        Container(
+          width: _casillaSize,
+          height: _casillaSize,
+          alignment: Alignment.center,
+          child: Text(
+            String.fromCharCode(65 + j - 1),
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      );
+    }
+    return Row(children: coordenadas);
+  }
 
+  Widget _buildFilaCasillas(int rowIndex) {
+    List<Widget> casillas = [];
+    // Etiqueta de fila
+    casillas.add(
+      Container(
+        width: _casillaSize,
+        height: _casillaSize,
+        alignment: Alignment.center,
+        child: Text(
+          (rowIndex + 1).toString(),
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+    // Casillas del tablero
     for (int j = 0; j < _numColumnas; j++) {
       casillas.add(Container(
-        width: _casillaSize, // Ancho de la casilla
-        height: _casillaSize, // Altura de la casilla
+        width: _casillaSize,
+        height: _casillaSize,
         decoration: BoxDecoration(
-          color: const Color.fromARGB(128, 116, 181, 213), // Color de fondo de la casilla con opacidad reducida
-          border: Border.all(color: Colors.black, width: 1), // Borde negro grueso
+          color: const Color.fromARGB(128, 116, 181, 213),
+          border: Border.all(color: Colors.black, width: 1),
         ),
       ));
     }
-
     return Row(children: casillas);
   }
 
-  Offset _boundPosition(Offset newPosition, double barcoSize, bool esRotado) {
-    double x, y;
+  Offset _boundPosition(Offset newPosition, double altoBarco, double anchoBarco) {
+    double x = (newPosition.dx / _casillaSize).round() * _casillaSize;
+    double y = (newPosition.dy / _casillaSize).round() * _casillaSize;
 
-    if (!esRotado) {
-      x = newPosition.dx.clamp(0.0, _boardSize - barcoSize);
-      y = newPosition.dy.clamp(0.0, _boardSize - _casillaSize);
-    } else {
-      x = newPosition.dx.clamp(0.0, _boardSize - _casillaSize);
-      y = newPosition.dy.clamp(0.0, _boardSize - barcoSize);
-    }
+    x = x.clamp(0.0, _boardSize - anchoBarco);
+    y = y.clamp(0.0, _boardSize - altoBarco);
 
-    return Offset(x, y);
+    return Offset(x + _casillaSize, y + _casillaSize);
   }
 }
