6351807e325bca05dac4fa59e168324de105ec87
Snatilla <isantilla11@gmail.com>
2024-04-08 18:20:40 +0200

    Partida Routes para pruebas Javi

--- src/routes/partidaRoutes.js
+++ src/routes/partidaRoutes.js
@@ -2,17 +2,42 @@ const express = require('express');
 const router = express.Router();
 const partidaController = require('../controllers/partidaController');
 const verificarToken = require('../middlewares/authjwt');
-const verificarRegistro = require('../middlewares/verificarRegistro');
 
-// Ruta para realizar disparo
-router.post('/realizarDisparo', partidaController.realizarDisparo);
 
-// Ruta para obtener partida
-router.post('/actualizarEstadoPartida', partidaController.actualizarEstadoPartida);
+// -------------------------------------------- //
+// -------------- PARTIDA BASICA -------------- //
+// -------------------------------------------- //
 
 // Ruta para crear partida
-router.post('/crearPartida', partidaController.crearPartida);
+router.post('/crearPartida', verificarToken, partidaController.crearPartida);
 
-// Ruta para 
+// Ruta para realizar disparo
+router.post('/realizarDisparo', verificarToken, partidaController.realizarDisparo);
+
+// Ruta para mostrar mi tablero
+router.post('/mostrarMiTablero', verificarToken, partidaController.mostrarMiTablero);
+
+// Ruta para mostrar tablero enemigo
+router.post('/mostrarTableroEnemigo', verificarToken, partidaController.mostrarTableroEnemigo);
+
+// Ruta para mostrar ambos tableros
+router.post('/mostrarTableros', verificarToken, partidaController.mostrarTableros);
+
+// Ruta para actualizar la partida (DEPRECATED)
+router.post('/actualizarPartida', verificarToken, partidaController.actualizarEstadoPartida);
+
+// Ruta para actualizar las estadisticas al terminar
+router.post('/actualizarEstadisticas', verificarToken, partidaController.actualizarEstadisticasFinales);
+
+
+// --------------------------------------------- //
+// -------------- CHAT DE PARTIDA -------------- //
+// --------------------------------------------- //
+
+// Ruta para obtener el chat
+router.post('/obtenerChat', verificarToken, partidaController.obtenerChat);
+
+// Ruta para enviar mensaje
+router.post('/enviarMensaje', verificarToken, partidaController.enviarMensaje);
 
 module.exports = router;
--- src/routes/perfilRoutes.js
+++ src/routes/perfilRoutes.js
@@ -11,9 +11,9 @@ const verificarRegistro = require('../middlewares/verificarRegistro');
 // Ruta para obtener perfil
 router.post('/obtenerUsuario', perfilController.obtenerUsuario);
 // Ruta para obtener datos personales del perfil
-router.post('/obtenerDatosPersonales', perfilController.obtenerPerfilDatosPersonales);
+router.post('/obtenerDatosPersonales', verificarToken, perfilController.obtenerPerfilDatosPersonales);
 // Ruta para modificar datos personales del perfil
-router.post('/modificarDatosPersonales', perfilController.modificarPerfilDatosPersonales);
+router.post('/modificarDatosPersonales', verificarToken, perfilController.modificarPerfilDatosPersonales);
 // Ruta para eliminar usuario
 router.post('/eliminarUsuario', perfilController.eliminarUsuario);
 
@@ -33,16 +33,16 @@ router.post('/inicioSesion', perfilController.iniciarSesion);
 // Ruta para modificar mazo
 router.post('/modificarMazo', verificarToken, perfilController.modificarMazo);
 // Ruta para modificar tablero
-router.post('/moverBarcoInicial', perfilController.moverBarcoInicial);
+router.post('/moverBarcoInicial', verificarToken, perfilController.moverBarcoInicial);
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 /*--------------------------------------------- PERFIL POST PARTIDA  -------------------------------------------------*/
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 // Ruta para modificar estadisticas
-router.post('/actualizarEstadisticas', perfilController.actualizarEstadisticas);
+router.post('/actualizarEstadisticas', verificarToken, perfilController.actualizarEstadisticas);
 // Ruta para modificar la experiencia
-router.post('/actualizarPuntosExperiencia', perfilController.actualizarEstadisticas);
+router.post('/actualizarPuntosExperiencia', verificarToken, perfilController.actualizarEstadisticas);
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 /*--------------------------------------------------- RED SOCIAL  ----------------------------------------------------*/
@@ -51,12 +51,12 @@ router.post('/actualizarPuntosExperiencia', perfilController.actualizarEstadisti
 // Ruta para obtener amigos
 router.post('/obtenerAmigos', perfilController.obtenerAmigos);
 // Ruta para añadir amigos
-router.post('/agnadirAmigo', perfilController.agnadirAmigo);
+router.post('/agnadirAmigo', verificarToken, perfilController.agnadirAmigo);
 // Ruta para eliminar amigos
-router.post('/eliminarAmigo', perfilController.eliminarAmigo);
+router.post('/eliminarAmigo', verificarToken, perfilController.eliminarAmigo);
 // Ruta para enviar solicitudes de amistad
-router.post('/enviarSolicitud', perfilController.enviarSolicitudAmistad);
+router.post('/enviarSolicitud', verificarToken, perfilController.enviarSolicitudAmistad);
 // Ruta para eliminar solicitudes de amistad
-router.post('/eliminarSolicitud', perfilController.eliminarSolicitudAmistad);
+router.post('/eliminarSolicitud', verificarToken, perfilController.eliminarSolicitudAmistad);
 
 module.exports = router;
